a9a9d94573e744e5b0b53b82af93d296e9a2ffee
Austin Joiner <austin.j.joiner1@gmail.com>
2023-01-20 16:10:04 -0500

    Finished JavaScript with HTML exercises.

---
+++ external_js/js/external.js
@@ -0,0 +1,30 @@
+console.log("Hello from external JavaScript.");
+
+alert("Welcome to my Website!");
+
+    var userColor = prompt("What is your favorite color?");
+        alert("WOW! " + (userColor) + " is my favorite color too.");
+
+    var questionOne = prompt("10 - 2 * 8 + 16 / 2");
+        if (questionOne == 2) {
+            alert("you are correct.");
+        } else {
+            alert("you are wrong. The answer is 2.");
+        }
+
+    var questionTwo = prompt("6 % 5");
+        if (questionTwo == 1) {
+            alert("you are correct.")
+        } else {
+            alert("you are wrong. The answer is 1.");
+        }
+
+    var questionThree = prompt("6 % 4");
+        if (questionThree == 2) {
+            alert("you are correct.");
+        } else {
+            alert("you are wrong. The answer is 2.");
+        }
+
+
+
