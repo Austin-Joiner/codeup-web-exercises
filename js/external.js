fed003233310de7e8e78188b802c772cd37a7628
Austin Joiner <austin.j.joiner1@gmail.com>
2023-01-20 15:24:52 -0500

    Finished If/else statement for grubhub link.

--- js/external.js
+++ js/external.js
@@ -8,4 +8,10 @@ var trueConfirm = confirm("Do you like to Code?");
 var userInput = prompt("Please enter name");
 console.log("The user entered: " + userInput);
 var haveLunch = confirm("did you eat lunch yet?");
-    if haveLunch
+    if (haveLunch === false) {
+       location.href = "https://www.grubhub.com/";
+       console.log((userInput) + (" did not have lunch yet, so ") + (userInput) + (" has been sent to grubhub."));
+    } else {
+        alert("I hope it was good!");
+        console.log(userInput + " has already had lunch.");
+    }
