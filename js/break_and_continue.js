18cf340d1ffef1553068d4d8d5ca0779f4f45657
Austin Joiner <austin.j.joiner1@gmail.com>
2023-01-25 12:20:49 -0500

    finished break and continue and for loops exercises.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,30 +2,37 @@
 
 
 
-
-
     var startTest = breakContinue();
+    //var userInput = parseInt(prompt("Pick an odd number."));
+
 
 function breakContinue() {
     var userInput = parseInt(prompt("Pick an odd number."));
-    while (!userInput || userInput % 2 === 0 || (userInput < 1 || userInput > 50)) {
 
-        if ((userInput % 2 !== 0) && (userInput >= 1 && userInput < 50)) {
-          console.log(userInput + " that is an odd number good job.");
-            alert(userInput + " that is an odd number good job.");
 
-            break;
+
+    while (userInput % 2 === 0 || userInput < 1 || userInput > 50 || isNaN(userInput)) {
+
+        if (false) {
+        break;
         }
-        console.log(userInput + " that is an even number try again.");
+        userInput = parseInt(prompt("I said pick a odd number, 1-50, you chose " + userInput + "."));
     }
+
+
+
+
+    alert("Good Job! " + userInput + " is an odd number.");
+    console.log(userInput + " is the number to skip.");
     for (var i = 1; i <= 50; i++) {
 
         if (i % 2 === 0) {
             continue;
-        } else if (userInput === false) {
+        } else if (userInput === i) {
+            console.log("WOAH! Skipping number: " + userInput);
             continue;
-
         }
-        console.log(i + " is an odd number.");
+        console.log("Here is an odd number: " + i);
+
     }
 }
