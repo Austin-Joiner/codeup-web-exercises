954323eaa292bcc3a852f7c28e4e12d512744510
Austin Joiner <austin.j.joiner1@gmail.com>
2023-02-13 11:24:27 -0500

    created a function that checks to see which user has the biggest email.

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -61,5 +61,17 @@ console.log(yearsTotal);
 var averageYear = yearsTotal / users.length;
 console.log(averageYear);
 
+const emailKing = users.reduce((email, user) => {
+   if (user.email.length > email.length) {
+       console.log(`I, ${user.email} am the biggest so far!`)
+       return user.email;// this log whenever an email is bigger than the previous since it loops through all emails
+   }
+   console.log(`I, ${user.email} am not the biggest`)
+    return email; // this logs the one that's not the biggest
+}, ''); // you need this otherwise it will log the whole object
+console.log(`${emailKing} is the KING of all Emails`);
+
+
+
 
 
