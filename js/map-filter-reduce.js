bfcf654fe61a263d53b62540e1cabfb2858724e5
Austin Joiner <austin.j.joiner1@gmail.com>
2023-02-13 10:49:13 -0500

    made a fucntion using es6 that uses filter.

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,51 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+//a function that creates a copy of the array of obj above that only includes(filters) users info with 3 or more languages.
+// old way
+// const newLang = users.filter(function(lang) {
+//    return lang.languages.length > 2;
+// });
+
+// es6
+const newLang = users.filter(lang => lang.languages.length > 2);
+newLang;
+
+
+
+
