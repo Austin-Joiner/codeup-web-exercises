6b8a7a2975a3b6f65726cf5e2cc1b9965e958f2e
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-10 17:13:28 -0500

    added index 0 to weathers since its in an array and MPH to finish winds measurment.

--- js/weather_map.js
+++ js/weather_map.js
@@ -8,8 +8,8 @@ $.get("http://api.openweathermap.org/data/2.5/weather", {
     '<h4>Temperature: ' + data.main.temp + '°F' +
          '<p>Feels Like: ' + data.main.feels_like + '</p>' +
          '<h4>Humidity: ' + data.main.humidity + '%</h4>' +
-         '<h4>Weather: ' + data.weather.main + '</h4>' +
-         '<h4>Wind: ' + data.wind.speed + '</h4>';
+         '<h4>Weather: ' + data.weather[0].main + '</h4>' +
+         '<h4>Wind: ' + data.wind.speed + 'MPH</h4>';
 
 
      $('#forecast').html(html);
