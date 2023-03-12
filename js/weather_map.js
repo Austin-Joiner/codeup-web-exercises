77c59e481b79dc6a0f779d03e576e08722c8ff57
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-12 15:14:50 -0400

    put get request in a function and called on it at the end of both geocodes to change the forecast and condense code.

--- js/weather_map.js
+++ js/weather_map.js
@@ -76,7 +76,11 @@ geocode("Tallahassee, FL", mapboxgl.accessToken).then(function (result) {
         console.log("longitude: " + lonInput);
         console.log("latitude: " + latInput);
         console.log("you dragged to: " + marker.getLngLat())
+        runForecast();
     });
+
+
+
 });
 
 
@@ -117,41 +121,7 @@ function searchGeocode(search, token) {
 
             });
         });
-        $.get("https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=" + latInput + "&lon=" + lonInput + "&appid=" + OPEN_WEATHER_APPID)
-            // {lon: -84.2807, lat: 30.4383}
-            .done(function (data) {
-                console.log(data);
-
-
-                var forecastInfo = data.list;
-                var html = '';
-
-                for (var i = 0; i < 5; i++) {
-
-
-                    html += '<div class="container-wrap card col-md-2">';
-                    html += '<div class="date-wrap">';
-                    html += '<h4></h4>'
-                    html += '</div>';
-                    html += '<div class="temp-wrap">';
-                    html += '<h4 class="temp">Temperature: ' + forecastInfo[i].main.temp + '°F' + '</h4>';
-                    html += '</div>';
-                    html += '<div class="humid-wrap">';
-                    html += '<h4 class="humid">Humidity: ' + forecastInfo[i].main.humidity + '%</h4>';
-                    html += '</div>';
-                    html += '<div class="weather-wrap">';
-                    html += '<div class="forecast-icon">';
-                    html += '<img src="https://openweathermap.org/img/w/' + forecastInfo[i].weather[0].icon + '.png"></div>';
-                    html += '<h4 class="weather">Weather: ' + forecastInfo[i].weather[0].main + '</h4>';
-                    html += '</div>';
-                    html += '<div class="wind-wrap">';
-                    html += '<h4 class="wind">Wind: ' + forecastInfo[i].wind.speed + 'MPH</h4>';
-                    html += '</div>';
-                    html += '</div>';
-
-                }
-                $('#forecast').html(html);
-            });
+        runForecast();
     });
 }
 
@@ -168,4 +138,43 @@ submitButton.addEventListener('click', function () {
 
 
 
+
+
+function runForecast() {
+    $.get("https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=" + latInput + "&lon=" + lonInput + "&appid=" + OPEN_WEATHER_APPID)
+        // {lon: -84.2807, lat: 30.4383}
+        .done(function (data) {
+            console.log(data);
+
+
+            var forecastInfo = data.list;
+            var html = '';
+
+            for (var i = 0; i < 5; i++) {
+
+
+                html += '<div class="container-wrap card col-md-2">';
+                html += '<div class="date-wrap">';
+                html += '<h4></h4>'
+                html += '</div>';
+                html += '<div class="temp-wrap">';
+                html += '<h4 class="temp">Temperature: ' + forecastInfo[i].main.temp + '°F' + '</h4>';
+                html += '</div>';
+                html += '<div class="humid-wrap">';
+                html += '<h4 class="humid">Humidity: ' + forecastInfo[i].main.humidity + '%</h4>';
+                html += '</div>';
+                html += '<div class="weather-wrap">';
+                html += '<div class="forecast-icon">';
+                html += '<img src="https://openweathermap.org/img/w/' + forecastInfo[i].weather[0].icon + '.png"></div>';
+                html += '<h4 class="weather">Weather: ' + forecastInfo[i].weather[0].main + '</h4>';
+                html += '</div>';
+                html += '<div class="wind-wrap">';
+                html += '<h4 class="wind">Wind: ' + forecastInfo[i].wind.speed + 'MPH</h4>';
+                html += '</div>';
+                html += '</div>';
+
+            }
+            $('#forecast').html(html);
+        });
+}
 
