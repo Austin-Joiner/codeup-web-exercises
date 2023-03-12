109a44c1c915704cb93e1744c8035e1671d39fcd
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-12 11:35:00 -0400

    added icons for weather cycling through forecastInfo and grabbing weather data.

--- js/weather_map.js
+++ js/weather_map.js
@@ -11,6 +11,9 @@ $.get("https://api.openweathermap.org/data/2.5/forecast?q=Tallahassee, US&units=
 
 
             html += '<div class="container-wrap">';
+            html += '<div class="date-wrap">';
+            html += '<h4></h4>'
+            html += '</div>';
             html += '<div class="temp-wrap">';
             html += '<h4 class="temp">Temperature: ' + forecastInfo[i].main.temp + '°F' + '</h4>';
             html += '</div>';
@@ -18,6 +21,8 @@ $.get("https://api.openweathermap.org/data/2.5/forecast?q=Tallahassee, US&units=
             html += '<h4 class="humid">Humidity: ' + forecastInfo[i].main.humidity + '%</h4>';
             html += '</div>';
             html += '<div class="weather-wrap">';
+            html += '<div class="forecast-icon">';
+            html += '<img src="https://openweathermap.org/img/w/' + forecastInfo[i].weather[0].icon + '.png"></div>';
             html += '<h4 class="weather">Weather: ' + forecastInfo[i].weather[0].main + '</h4>';
             html += '</div>';
             html += '<div class="wind-wrap">';
