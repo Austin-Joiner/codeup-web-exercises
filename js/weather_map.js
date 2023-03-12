d76cd96de2ebece26e82825ee508e71769537794
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-12 11:09:57 -0400

    added forecast link and refactored get request info into the link.

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,19 +1,23 @@
-$.get("http://api.openweathermap.org/data/2.5/weather", {
-    APPID: OPEN_WEATHER_APPID,
-    q:     "Tallahassee, US"
-}).done(function(data) {
+$.get("https://api.openweathermap.org/data/2.5/forecast?q=Tallahassee, US&units=imperial&appid=" + OPEN_WEATHER_APPID)
+// {lon: -84.2807, lat: 30.4383}
+.done(function(data) {
     console.log(data);
 
-     var html =
 
-    '<div class="container-wrap row"><div class="temp-wrap col-md-6 card">' +
-         '<h4 class="temp">Temperature: ' + data.main.temp + '°F' +
-         '<p class="feels_like">Feels Like: ' + data.main.feels_like + '</p></div>' +
-         '<div class="humid-wrap col-md-6 card"><h4 class="humid">Humidity: ' + data.main.humidity + '%</h4></div>' +
-         '<div class="weather-wrap col-md-6 card"><h4 class="weather">Weather: ' + data.weather[0].main + '</h4></div>' +
-         '<div class="wind-wrap col-md-6 card"><h4 class="wind">Wind: ' + data.wind.speed + 'MPH</h4></div></div>';
+    var forecastInfo = data.list;
+     var html = '';
+
+         for ( var i = 0; i < 5; i++) {
 
 
+             html += '<div class="container-wrap"><div class="temp-wrap">';
+             html += '<h4 class="temp">Temperature: ' + forecastInfo[i].main.temp + '°F' +
+             '<p class="feels_like">Feels Like: ' + forecastInfo[i].main.feels_like + '</p></div>';
+             html += '<div class="humid-wrap"><h4 class="humid">Humidity: ' + forecastInfo[i].main.humidity + '%</h4></div>';
+             html += '<div class="weather-wrap"><h4 class="weather">Weather: ' + forecastInfo[i].weather[0].main + '</h4></div>';
+             html += '<div class="wind-wrap"><h4 class="wind">Wind: ' + forecastInfo[i].wind.speed + 'MPH</h4></div></div>';
+
+         }
      $('#forecast').html(html);
 
 
