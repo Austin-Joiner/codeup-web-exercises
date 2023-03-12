877226725cff83bb75742f945947a3b0392e7e33
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-12 16:33:37 -0400

    fixed starting origin coordinates and added some bootstrap grid to the layout.

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,4 +1,4 @@
-$.get("https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=-84.280933&lon=30.438083&appid=" + OPEN_WEATHER_APPID)
+$.get("https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=30.438083&lon=-84.280933&appid=" + OPEN_WEATHER_APPID)
     // {lon: -84.2807, lat: 30.4383}
     .done(function (data) {
         console.log(data);
@@ -10,7 +10,7 @@ $.get("https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=-84.2
         for (var i = 0; i < 5; i++) {
 
 
-            html += '<div class="container-wrap card col-md-2">';
+            html += '<div class="container-wrap card col-12 col-md-2 ">';
             html += '<div class="date-wrap">';
             html += '<h4></h4>'
             html += '</div>';
@@ -37,6 +37,10 @@ $.get("https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=-84.2
 
 var lonInput = -84.280933;
 var latInput = 30.438083;
+var marker = document.getElementsByClassName('mapboxgl-marker');
+
+
+
 
 accessToken = MAPBOX_KEY
 mapboxgl.accessToken = accessToken;
@@ -78,103 +82,115 @@ geocode("Tallahassee, FL", mapboxgl.accessToken).then(function (result) {
         console.log("you dragged to: " + marker.getLngLat())
         runForecast();
     });
-
-
-
-});
-
-
-var searchGeo = document.querySelector('#search-geo');
-var submitButton = document.querySelector('#submit');
-var grabCoordinates;
-
-
-function searchGeocode(search, token) {
-    var searchInput = searchGeo.value
-
-
-    geocode(searchInput, token).then(function (coordinates) {
-
-        let marker = new mapboxgl.Marker()
-            .setLngLat(coordinates)
-            .addTo(map)
-        grabCoordinates = coordinates;
-        lonInput = grabCoordinates[0];
-        latInput = grabCoordinates[1];
-        console.log(grabCoordinates);
-        console.log("longitude: " + lonInput);
-        console.log("latitude: " + latInput);
-
+    marker.getElement().addEventListener('click', function () {
 
         map.flyTo({
-            center: coordinates,
+            center: marker.getLngLat(),
             zoom: 10,
-            speed: 1,
-        });
+            speed: 1
+        })
 
-        marker.getElement().addEventListener('click', function () {
 
-            map.flyTo({
-                center: coordinates,
-                zoom: 17,
-                speed: 1
-
-            });
-        });
-        runForecast();
     });
-}
 
-
-submitButton.addEventListener('click', function () {
-    var searchInput = searchGeo.value;
-
-
-    searchGeocode(searchInput, MAPBOX_KEY);
 });
 
 
+    var searchGeo = document.querySelector('#search-geo');
+    var submitButton = document.querySelector('#submit');
+    var grabCoordinates;
 
 
+    function searchGeocode(search, token) {
+        var searchInput = searchGeo.value
+        geocode(searchInput, token).then(function (coordinates) {
+
+            var marker = new mapboxgl.Marker({draggable: true})
+                .setLngLat(coordinates)
+                .addTo(map)
+
+            grabCoordinates = coordinates;
+            lonInput = grabCoordinates[0];
+            latInput = grabCoordinates[1];
+            console.log("longitude: " + lonInput);
+            console.log("latitude: " + latInput);
+
+            marker.on('dragend', function () {
+
+                lonInput = marker.getLngLat().lng;
+                latInput = marker.getLngLat().lat;
+                console.log("you dragged to: " + marker.getLngLat())
+                runForecast();
+
+            });
+
+            map.flyTo({
+                center: marker.getLngLat(),
+                zoom: 10,
+                speed: 1,
 
 
+            });
 
+            marker.getElement().addEventListener('click', function () {
 
-function runForecast() {
-    $.get("https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=" + latInput + "&lon=" + lonInput + "&appid=" + OPEN_WEATHER_APPID)
-        // {lon: -84.2807, lat: 30.4383}
-        .done(function (data) {
-            console.log(data);
+                map.flyTo({
+                    center: marker.getLngLat(),
+                    zoom: 10,
+                    speed: 1
 
-
-            var forecastInfo = data.list;
-            var html = '';
-
-            for (var i = 0; i < 5; i++) {
-
-
-                html += '<div class="container-wrap card col-md-2">';
-                html += '<div class="date-wrap">';
-                html += '<h4></h4>'
-                html += '</div>';
-                html += '<div class="temp-wrap">';
-                html += '<h4 class="temp">Temperature: ' + forecastInfo[i].main.temp + '°F' + '</h4>';
-                html += '</div>';
-                html += '<div class="humid-wrap">';
-                html += '<h4 class="humid">Humidity: ' + forecastInfo[i].main.humidity + '%</h4>';
-                html += '</div>';
-                html += '<div class="weather-wrap">';
-                html += '<div class="forecast-icon">';
-                html += '<img src="https://openweathermap.org/img/w/' + forecastInfo[i].weather[0].icon + '.png"></div>';
-                html += '<h4 class="weather">Weather: ' + forecastInfo[i].weather[0].main + '</h4>';
-                html += '</div>';
-                html += '<div class="wind-wrap">';
-                html += '<h4 class="wind">Wind: ' + forecastInfo[i].wind.speed + 'MPH</h4>';
-                html += '</div>';
-                html += '</div>';
-
-            }
-            $('#forecast').html(html);
+                });
+            });
+            runForecast();
         });
-}
+    }
 
+
+    submitButton.addEventListener('click', function () {
+        var searchInput = searchGeo.value;
+
+        for (var i = marker.length - 1; i >= 0; i--) {
+            marker[i].remove();
+        }
+        searchGeocode(searchInput, MAPBOX_KEY);
+    });
+
+
+    function runForecast() {
+        $.get("https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=" + latInput + "&lon=" + lonInput + "&appid=" + OPEN_WEATHER_APPID)
+            // {lon: -84.2807, lat: 30.4383}
+            .done(function (data) {
+                console.log(data);
+
+
+                var forecastInfo = data.list;
+                var html = '';
+
+                for (var i = 0; i < 5; i++) {
+
+
+                    html += '<div class="container-wrap card col-md-2">';
+                    html += '<div class="date-wrap">';
+                    html += '<h4></h4>'
+                    html += '</div>';
+                    html += '<div class="temp-wrap">';
+                    html += '<h4 class="temp">Temperature: ' + forecastInfo[i].main.temp + '°F' + '</h4>';
+                    html += '</div>';
+                    html += '<div class="humid-wrap">';
+                    html += '<h4 class="humid">Humidity: ' + forecastInfo[i].main.humidity + '%</h4>';
+                    html += '</div>';
+                    html += '<div class="weather-wrap">';
+                    html += '<div class="forecast-icon">';
+                    html += '<img src="https://openweathermap.org/img/w/' + forecastInfo[i].weather[0].icon + '.png"></div>';
+                    html += '<h4 class="weather">Weather: ' + forecastInfo[i].weather[0].main + '</h4>';
+                    html += '</div>';
+                    html += '<div class="wind-wrap">';
+                    html += '<h4 class="wind">Wind: ' + forecastInfo[i].wind.speed + 'MPH</h4>';
+                    html += '</div>';
+                    html += '</div>';
+
+                }
+                $('#forecast').html(html);
+            });
+
+    }
